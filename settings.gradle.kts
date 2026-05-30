5b0544ba6ea39b0197a20a11a725a29a3b57d713
Anders Sveen <anders@f12.no>
2026-05-30 21:45:05 +0200

    Fix build warnings: remove legacy Usage attributes (Kotlin 2.2.0 in test-project), add toolchain repositories, remove duplicate Maven source roots

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,7 @@
 rootProject.name = "code-navigator"
 
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version "0.10.0"
+}
+
 includeBuild("test-project")
--- test-project/settings.gradle.kts
+++ test-project/settings.gradle.kts
@@ -1,1 +1,5 @@
 rootProject.name = "test-project"
+
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version "0.10.0"
+}
